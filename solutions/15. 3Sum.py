96c30d17e62e6c9781f26b0093f037a0a47bf5bb
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2021-11-16 22:59:29 -1000

    1120 ms

--- solutions/15. 3Sum.py
+++ solutions/15. 3Sum.py
@@ -1,24 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result=[]
-        nums_=[]
-        for i in range(len(nums)):
-            if nums[i] not in nums_:
-                nums_.append(nums[i])
-            
-                temp={}
-                rem=0-nums[i]
-                for j in range(i+1, len(nums)):
-                    if nums[j] not in temp:
-                        temp[rem-nums[j]]=nums[j]
+        nums.sort()
+        print(nums)
+        for i in range(len(nums)-2):
+            if nums[i]!=nums[i-1] or i==0:
+                left=i+1
+                r=len(nums)-1
+                
+                while left<r:
+                    
+                    if (nums[left]+nums[r])<(0-nums[i]):
+                        left=left+1
+                        
+                    elif (nums[left]+nums[r])>(0-nums[i]):
+                        r=r-1
+                        
                     else:
-                        l_s=[nums[i],rem-nums[j],nums[j]]
-                        l_s.sort()
-                        if l_s not in result:
-                            result.append(l_s)
+                        if nums[left]!=nums[left-1] or left==i+1:
+                            result.append([nums[i],nums[left],nums[r]])
+                        
+                        
+                        
+                        left=left+1
+                        r=r-1
+              
         return result
-               
-                
-                
-                
-        
