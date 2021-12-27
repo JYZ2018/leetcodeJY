005c0aea879756f1845fbe6c024b9aa96fcdbb49
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2021-12-26 23:40:28 -1000

    20 ms

--- solutions/15. 3Sum.java
+++ solutions/15. 3Sum.java
@@ -1,25 +1,6 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        
-        Set<List<Integer>> result = new HashSet<>();
-        if (nums.length<3) {return new ArrayList<>();}
-        Arrays.sort(nums); 
-        
-        for (int i=0; i<nums.length-2; i++) {
-            int l=i+1, r=nums.length-1;
-            while (l<r) {
-            int sum=nums[i]+nums[l]+nums[r];
-            if (sum==0) {result.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                        l++; r--;}
-            else if (sum>0) {r--;}
-            else {l++;}
-        }
-        }
-        return new ArrayList(result);
-        
         
        
-        /* Arrays.sort(nums);
+        Arrays.sort(nums);
         
         List<List<Integer>> result= new ArrayList<>();
         if (nums.length<3) {return result;}
@@ -35,7 +16,7 @@ class Solution {
                     while (l<r && nums[l]==nums[l+1] ) {l++;}
                     while (l<r && nums[r]==nums[r-1]) {r--;}
                     l++; r--;
-                }
+                }
                     
                 else if (nums[i]+nums[l]+nums[r]<0) {
                     //while (l<r && nums[l]==nums[l+1] ) {l++;};
@@ -43,14 +24,28 @@ class Solution {
                 else { 
                     //while (l<r && nums[r]==nums[r-1]) {r--;};
                       r--;}
-                }      
-            }
-        }
+                }      
+            }
+        }
         
         return result;
-        */
+        
         
         
         /* List<List<Integer>> result= new ArrayList<>();
         Arrays.sort(nums);
         for (int i=0;  i<nums.length; i++) {
+           if ((i>0 && nums[i]!=nums[i-1])  || i==0) {
+           int l=i+1;
+           int r=nums.length-1;
+           while (l<r) {
+               //System.out.println("nums[i]+nums[1]+nums[r] is: "+(nums[i]+nums[l]+nums[r] ));
+               //System.out.println("nums[i] is: "+nums[i]+" nums[l] is: "+nums[l]+" nums[r] is: "+nums[r] );
+               if (nums[i]+nums[l]+nums[r]==0) {
+                  // System.out.println("l is: "+l+" r is : "+r);
+                   ArrayList<Integer> temp = new ArrayList<Integer>();
+                   if ((nums[l]!=nums[l-1]) ||  l==i+1) {
+                       //temp.add(nums[i]);
+                       //temp.add(nums[l]);
+                       //temp.add(nums[r]);
+                       result.add(Arrays.asList(nums[i],nums[l],nums[r]));
