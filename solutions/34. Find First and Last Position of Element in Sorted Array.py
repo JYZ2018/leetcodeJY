effeb8ed94c25b581b076d6b76e6a1588aaa54ca
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-02-09 11:27:27 -1000

    131 ms

--- solutions/34. Find First and Last Position of Element in Sorted Array.py
+++ solutions/34. Find First and Last Position of Element in Sorted Array.py
@@ -2,38 +2,37 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
         left,right=0,len(nums)-1
-        
+       
         res=[]
-        def binarysearch(left,right):
-            while left<=right:
-                mid=left+(right-left)//2
-                #print(mid)
-                if left==right and nums[mid]!=target:
-                    return
-                
-                elif target>nums[mid]:
-                    left=mid+1
-                elif target<nums[mid]:
-                    right=mid-1
-                
-                else:
-                   
-                    if nums[mid]==nums[left] and nums[mid]==nums[right]:
-                        res.append(left)
-                        res.append(right)
-                        return
-                    elif nums[left]<nums[mid]:
-                        left+=1
-                    
-                    elif nums[right]>nums[mid]:
-                        right-=1
+        
+        while left<=right:
+            #print(left,right)
+            #print(res)
+            mid=left+(right-left)//2
+            #print(mid)
             
-        binarysearch(left,right)
-        #print(res)
-        if len(res)==0:
-            return [-1,-1]
-        else:
-            return res
+                
+            if target>nums[mid]:
+                left=mid+1
+            elif target<nums[mid]:
+                right=mid-1
+                
+            else:
+                   
+                if nums[left]==nums[right]:
+                    res.append(left)
+                    res.append(right)
+                    return res
+                    
+                elif nums[left]<nums[mid]:
+                    left+=1
+                    
+                elif nums[right]>nums[mid]:
+                    right-=1
+            #print("2:",left,right)
+        return [-1,-1]
+            
+        
         
             
             
