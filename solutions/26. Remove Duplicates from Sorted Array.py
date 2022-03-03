698638bea2d9316eb1d3b700bd1c089a880696d7
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-03-03 09:18:24 -1000

    218 ms

--- solutions/26. Remove Duplicates from Sorted Array.py
+++ solutions/26. Remove Duplicates from Sorted Array.py
@@ -5,6 +5,27 @@ class Solution:
             if nums[i]!=nums[j]:
                 j+=1
                 nums[j]=nums[i]
-        nums[:]=nums[:j+1]
+                #print(j)
+        return j+1
+        
+        
+        
+#         j=0
+#         for i in range(len(nums)):
+#             if nums[i]!=nums[j]:
+#                 nums[j+1]=nums[i]
+#                 j+=1
+#         return j+1
        
+            
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
