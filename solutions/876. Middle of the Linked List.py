6fd2b534f988e323d40a1a152cdd7d28d0ce5ef1
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-03-22 21:13:47 -1000

    49 ms

--- solutions/876. Middle of the Linked List.py
+++ solutions/876. Middle of the Linked List.py
@@ -5,15 +5,27 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        n=0
-        dummy1,dummy2=head,head
-        while dummy1:
-            n+=1
-            dummy1=dummy1.next
-        mid=n//2
-        for i in range(mid):
-            dummy2=dummy2.next
-        return dummy2
+        
+        
+        # slow and fast pointer
+        slow, fast=head,head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        
+        
+        
+        #my own messy code
+#         n=0
+#         dummy1,dummy2=head,head
+#         while dummy1:
+#             n+=1
+#             dummy1=dummy1.next
+#         mid=n//2
+#         for i in range(mid):
+#             dummy2=dummy2.next
+#         return dummy2
             
         
         
