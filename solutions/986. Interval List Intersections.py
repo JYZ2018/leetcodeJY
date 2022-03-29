fdf50a2898c26b731539bb48b0d63deb81a8abcf
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-03-28 23:03:22 -1000

    199 ms

--- solutions/986. Interval List Intersections.py
+++ solutions/986. Interval List Intersections.py
@@ -1,23 +1,37 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
-        # same method as meeting room 2
         
-        
-        start=sorted([i[0] for i in firstList]+[i[0] for i in secondList])
-        end=sorted([i[1] for i in firstList]+[i[1] for i in secondList])
-        #print(start,end)
         res=[]
-        count=0
-        s,e=0,0
-        while s<len(start):
-            if start[s]<=end[e]:
-                s+=1
-                count+=1
+        i,j=0,0
+        while i<len(firstList) and j<len(secondList):
+            start=max(firstList[i][0],secondList[j][0])
+            end=min(firstList[i][1],secondList[j][1])
+            if start<=end:
+                res.append([start,end])
+            if firstList[i][1]>=secondList[j][1]:
+                j+=1
             else:
-                e+=1
-                count-=1
-            if count==2:
-                res.append([start[s-1],end[e]])
+                i+=1
         return res
         
+        
+        
+        
+        # same method as meeting room 2
+        
+        '''
+        start=sorted([i[0] for i in firstList]+[i[0] for i in secondList])
+        end=sorted([i[1] for i in firstList]+[i[1] for i in secondList])
+        #print(start,end)
+        res=[]
+        count=0
+        s,e=0,0
+        while s<len(start):
+            if start[s]<=end[e]:
+                s+=1
+                count+=1
+            else:
+                e+=1
+                count-=1
+            if count==2:
