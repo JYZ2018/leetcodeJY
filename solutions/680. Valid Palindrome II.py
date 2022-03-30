d27fbf38ab566aa91d9cc6bf74e65b80c4854bf4
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-03-29 22:41:00 -1000

    339 ms

--- solutions/680. Valid Palindrome II.py
+++ solutions/680. Valid Palindrome II.py
@@ -1,9 +1,36 @@
 class Solution:
+   
     def validPalindrome(self, s: str) -> bool:
+        def isvalid(s):
+            l,r=0,len(s)-1
+            while l<r:
+                if s[l]==s[r]:
+                    l+=1
+                    r-=1
+                else:
+                    return False
+            return True
+            
+     
         l,r=0,len(s)-1
         while l<r:
             if s[l]==s[r]:
-                l+=1;r-=1
+                l+=1
+                r-=1
             else:
-                return s[l:r]==s[l:r][::-1] or s[l+1:r+1]==s[l+1:r+1][::-1]
+                return isvalid(s[l+1:r+1]) or isvalid(s[l:r])
         return True
+                  
+                
+        
+        
+        
+        
+        
+        
+        '''
+        l,r=0,len(s)-1
+        while l<r:
+            if s[l]==s[r]:
+                l+=1;r-=1
+            else:
