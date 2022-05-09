59f88534f18a88e8b7f249c6e9bac598807a7b59
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-05-08 21:44:31 -1000

    0 ms

--- solutions/21. Merge Two Sorted Lists.java
+++ solutions/21. Merge Two Sorted Lists.java
@@ -10,25 +10,56 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy =new ListNode (0);
-        ListNode curr=dummy;
+//         ListNode dummy =new ListNode (0);
+//         ListNode curr=dummy;
         
         
-        while (list1!=null & list2!=null) {
+//         while (list1!=null & list2!=null) {
+//             if (list1.val<list2.val) {
+//                 curr.next=list1;
+//                 list1=list1.next;
+//              }
+//             else {
+//                 curr.next=list2;
+//                 list2=list2.next;
+//             }
+//             curr=curr.next;
+        
+//         }
+   
+//     if (list1!=null) curr.next=list1;
+//     else curr.next=list2;
+//     return dummy.next;
+        
+        ListNode res;
+        if (list1==null) return list2;
+        if (list2==null) return list1;
+        
+        
+        if (list1.val<list2.val) {
+            res=list1;
+            list1=list1.next;
+        }
+        else {
+            res=list2;
+            list2=list2.next;
+        }
+        
+        ListNode curr=res;
+        while (list1!=null && list2!=null) {
             if (list1.val<list2.val) {
                 curr.next=list1;
-                list1=list1.next;
-             }
+                list1=list1.next; 
+            }
             else {
-                curr.next=list2;
-                list2=list2.next;
+               curr.next=list2;
+               list2=list2.next;  
             }
             curr=curr.next;
-        
         }
-   
-    if (list1!=null) curr.next=list1;
+        
+     if (list1!=null) curr.next=list1;     
     else curr.next=list2;
-    return dummy.next;
+        return res;
 }
 }
