3877cb68915fd96375cfba735f0c8a36a13e0109
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-05-14 23:09:37 -1000

    23 ms

--- solutions/147. Insertion Sort List.java
+++ solutions/147. Insertion Sort List.java
@@ -10,24 +10,48 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode dummy = new ListNode (0);
-        ListNode curr = head;
-        
-        while (curr!=null) {
-            ListNode prev = dummy;
-            while (prev.next!=null && prev.next.val<=curr.val) {
-                prev=prev.next;
-            }
-            
-            ListNode next=curr.next;
-            
-            curr.next=prev.next;
-            prev.next=curr;
-            
-            curr=next;   
-            
+        // recursion
+        if (head==null || head.next==null) {
+            return head;
         }
-        return dummy.next;
-   
-    }
-}
+        
+        head.next=insertionSortList(head.next);
+        
+        
+        ListNode first = new ListNode(head.val);
+//         ListNode temp0 =head;
+//         while (temp0!=null) {
+//             System.out.println("0 "+temp0.val);
+//             temp0=temp0.next;
+//         }
+        
+        
+        ListNode curr =head;
+        
+        while (curr.next!=null && curr.next.val<=first.val) {
+                 curr=curr.next;
+            }
+        
+        ListNode next = curr.next;    
+        curr.next=first;
+        first.next = next;
+        // ListNode temp =head;
+        // while (temp!=null) {
+        //     System.out.println(temp.val);
+        //     temp=temp.next;
+        // }
+        //System.out.println();
+        return head.next;
+        
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
