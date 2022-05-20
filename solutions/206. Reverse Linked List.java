74fab71263231f845adf123fbf3a1e182bd14725
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-05-19 16:36:41 -1000

    0 ms

--- solutions/206. Reverse Linked List.java
+++ solutions/206. Reverse Linked List.java
@@ -9,24 +9,49 @@
  * }
  */
 class Solution {
+    
+    
     public ListNode reverseList(ListNode head) {
-    ListNode prev=null;
-    ListNode curr=head;
-    ListNode next=null;
+        if (head==null) return head;
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        
+        while (curr!=null) {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;   
+        }
+        
+        return prev;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // iterative
+//     ListNode prev=null;
+//     ListNode curr=head;
+//     ListNode next=null;
     
         
-    while (curr!=null) {
-        next=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=next;
-        
-        
-    }
-    //System.out.println(head);
-     return prev;
-        
-    }
-        
-    
-}
+//     while (curr!=null) {
+//         next=curr.next;
+//         curr.next=prev;
+//         prev=curr;
+//         curr=next;      
+//     }
