47d7677ac78d75bc7384a901ceba798ba734772a
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2022-06-08 20:04:56 -1000

    24 ms

--- solutions/49. Group Anagrams.java
+++ solutions/49. Group Anagrams.java
@@ -1,20 +1,45 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        Map<String,List<String>> lmap = new HashMap<>();
-        
+        Map<String, List<String>> lmap = new HashMap<>();
+        int[] count = new int[26];
         for (String str: strs) {
-            char[] sa = str.toCharArray();
-            Arrays.sort(sa);
-            String key = String.valueOf(sa);
-            if (!lmap.containsKey(key)) {
-                lmap.put(key, new ArrayList<String>());
-            } 
-            lmap.get(key).add(str);  
+            Arrays.fill(count,0);
+            for (char c: str.toCharArray()) {
+                count[c-'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder("");
+            for (int i=0; i<26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            
+            String key = sb.toString();
+            if (!lmap.containsKey(key)) lmap.put(key, new ArrayList<String>());
+            lmap.get(key).add(str);
+            
+            
         }
-        
-        //System.out.println(lmap);
-        
         return new ArrayList(lmap.values());
+        
+        
+        
+        
+        
+//         Map<String,List<String>> lmap = new HashMap<>();
+        
+//         for (String str: strs) {
+//             char[] sa = str.toCharArray();
+//             Arrays.sort(sa);
+//             String key = String.valueOf(sa);
+//             if (!lmap.containsKey(key)) {
+//                 lmap.put(key, new ArrayList<String>());
+//             } 
+//             lmap.get(key).add(str);  
+//         }
+        
+//         //System.out.println(lmap);
+        
+//         return new ArrayList(lmap.values());
     }
 }
